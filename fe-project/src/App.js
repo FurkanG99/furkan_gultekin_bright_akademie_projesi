cc59e24074fe87c36bdbe977586afd6ea745cbb0
FurkanG99 <furkan_gultekin00@hotmail.com>
2023-12-19 00:54:44 +0300

    Header Eklendi

---
+++ fe-project/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './Components/Header';
+import css from './Css/style.css'
+
+
+
+function App() {
+  return (
+   <>
+      <Header />    
+   </>
+  );
+}
+
+export default App;
---
+++ fe-project/src/Components/Header.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown'
+import Container from 'react-bootstrap/Container'
+import Button from 'react-bootstrap/esm/Button';
+import Form from 'react-bootstrap/Form';
+
+function Header() {
+  return (
+    <>
+     <Navbar expand="lg" className="bg-primary p-3" fixed='top'>
+      <Container>
+        <Navbar.Brand href="#" className='text-info fs-4 '>BAU-<span className='text-warning fs-4'>Bright</span></Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" className='text-white'/>
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="ms-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1"className='text-white m-2 nav-item fs-5'>BAU Bright Nedir</Nav.Link>
+            <Nav.Link href="#action2"className='text-white m-2 nav-item fs-5'>Neden Katılmalıyım</Nav.Link>
+            <Nav.Link href="#action2"className='text-white m-2 nav-item fs-5'>Yöntem</Nav.Link>
+            <Nav.Link href="#action2"className='text-white m-2 nav-item fs-5'>Süreç</Nav.Link>
+            <NavDropdown title={<span className="text-white fs-5">Eğitimler</span>} id="navbarScrollingDropdown" className='mt-2 me-5' style={{ minWidth: '150px' }} >
+              <NavDropdown.Item href="#fullstack-programlama-egitimi">FullStack Programlama Eğitimi</NavDropdown.Item>
+              <NavDropdown.Item href="#network-sistem-ve-siber-guvenlik-uzmanlık-egitimi">
+              Network, Sistem ve <br></br> Siber Güvenlik Uzmanlık Eğitimi
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#bulut-bilisim-uzmanlık-egitimi">
+              Bulut Bilişim Uzmanlık Eğitimi
+              </NavDropdown.Item> 
+              <NavDropdown.Item href="#full-data-uzmanlık-egitimi">
+              Full Data Uzmanlık Eğitimi
+              </NavDropdown.Item>
+            </NavDropdown>
+
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-light">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    </>
+  )
+}
+
+export default Header
