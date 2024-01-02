54c657bd81daaef03caf3157e0f41aec2f752481
FurkanG99 <furkan_gultekin00@hotmail.com>
2024-01-02 18:59:03 +0300

    why page

--- fe-project/src/Pages/WhyPage.js
+++ fe-project/src/Pages/WhyPage.js
@@ -1,10 +1,35 @@
 import React from 'react'
+import CSS from '../Css/why.css' 
+import  Accordion  from 'react-bootstrap/Accordion'
 
 const WhyPage = () => {
   return (
-    <div className='container-lg'>
-      asdasdasdasd
-    </div>
+    <Accordion defaultActiveKey="0" className='why-accordion mt-5 w-25 '>
+    <Accordion.Item eventKey="0">
+      <Accordion.Header>Accordion Item #1</Accordion.Header>
+      <Accordion.Body>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+        aliquip ex ea commodo consequat. Duis aute irure dolor in
+        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+        culpa qui officia deserunt mollit anim id est laborum.
+      </Accordion.Body>
+    </Accordion.Item>
+    <Accordion.Item eventKey="1">
+      <Accordion.Header>Accordion Item #2</Accordion.Header>
+      <Accordion.Body>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+        aliquip ex ea commodo consequat. Duis aute irure dolor in
+        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+        culpa qui officia deserunt mollit anim id est laborum.
+      </Accordion.Body>
+    </Accordion.Item>
+  </Accordion>
   )
 }
 
